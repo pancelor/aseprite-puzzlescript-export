b3efcea8afb257f4f750dd4950ca451f83fd1466
pancelor <pancelor@gmail.com>
2023-02-02 08:36:54 -0800

    cleanup, spacing; change renderTilemapCel iter

--- PuzzleScriptExport.lua
+++ PuzzleScriptExport.lua
@@ -3,7 +3,6 @@
 
 -- handy link to the aseprite api docs:
 -- https://www.aseprite.org/api/
--- https://github.com/aseprite/api/
 
 --[[
 note: this extenstion does not deal with making maps using the aseprite
@@ -22,24 +21,24 @@ the docs don't exist yet, but see https://github.com/aseprite/api/issues/66
 # libpance
 ]]
 
-local qindent=0
+local qindent = 0
 local function qq(...)
-  local tbl={...}
-  local s=""
-  for i=1,#tbl do
-    local o=tbl[i]
+  local tbl = {...}
+  local s = ""
+  for i = 1,#tbl do
+    local o = tbl[i]
     if type(o)=='table' then
-      s=s..'{\n'
-      qindent=qindent+2
+      s = s..'{\n'
+      qindent = qindent+2
       assert(qindent<50)
-      local tab=string.rep(" ",qindent)
+      local tab = string.rep(" ",qindent)
       for k,v in pairs(o) do
-        s=s..tab..k..'='..qq(v)..'\n'
+        s = s..tab..k..' = '..qq(v)..'\n'
       end
-      qindent=qindent-2
-      s=s..string.rep(" ",qindent)..'} '
+      qindent = qindent-2
+      s = s..string.rep(" ",qindent)..'} '
     else
-      s=s..tostring(o).." "
+      s = s..tostring(o).." "
     end
   end
   return s
@@ -58,7 +57,7 @@ local function find(tab,x)
   end
 end
 local function ifind(tab,x)
-  for i=1,#tab do
+  for i = 1,#tab do
     if tab[i]==x then return i end
   end
 end
@@ -67,7 +66,7 @@ local function add(tab,elem)
   table.insert(tab,elem)
   return elem
 end
-local function deli(tab,ix)
+local function deli(tab, ix)
   return table.remove(tab,ix)
 end
 
@@ -75,20 +74,21 @@ end
 # helpers
 ]]
 
--- this doesn't reset between exports
--- that's a good thing, I think
-_id=0
+-- note: this counter doesn't reset between exports
+-- (good, I think?)
+local _id = 0
 local function getId()
-  _id=_id+1
+  _id = _id+1
   return _id
 end
 
--- convert a single "zone" into a string
+-- converts a single "zone" into a string
 local function exportZone(img,zone)
+  assert(img.colorMode==ColorMode.RGB)
   -- pq("exportZone",zone)
 
-  local pal={} -- array of colors
-  local body=""
+  local pal = {} -- array of colors
+  local body = ""
   for it in img:pixels(zone.bounds) do
     -- pqt = it.x==0 and it.y==0 and pq or function()end
 
@@ -122,13 +122,13 @@ local function exportZone(img,zone)
     end
 
     body = body..code
-    if it.x == zone.bounds.width-1 then
+    if it.x==zone.bounds.width-1 then
       body = body.."\n"
     end
   end
 
   if #pal>10 then
-    print("error: more than 10 colors in sprite at x="..x.." y="..y)
+    print("error: more than 10 colors in sprite at ("..x..","..y..")")
   end
 
   if #pal>0 then
@@ -152,69 +152,10 @@ local function defaultGridType()
   end
 end
 
---[[
-# script
-]]
-
--- returns a list of "zones" that fit into the current selection of sprite
---   a "zone" is a {name=<string>,bounds=<Rectangle>} object
--- sel may be non-rectangular (e.g. multiple rectangles)
---   but support isn't great (e.g. the grid anchor will not reset between
---   multiple selections)
-local function gatherZones(sprite,gridtype)
-  local zones={}
-
-  local sel=sprite.selection
-  -- rect: the subrectangle to export tiles from
-  local rect=sel.isEmpty and sprite.bounds or sel.bounds
-  local zonew
-  local zoneh
-  if gridtype=="5x5" then
-    zonew=5
-    zoneh=5
-    -- if your selection isn't exact multiples of 5, extra tiles will be created
-    -- on the edge. these tiles will be a full 5x5
-  elseif gridtype=="aseprite grid" then
-    zonew=sprite.gridBounds.width
-    zoneh=sprite.gridBounds.height
-    -- selection correction
-    -- pq(rect)
-    local x1=math.floor(rect.x/zonew)*zonew
-    local y1=math.floor(rect.y/zoneh)*zoneh
-    local x2=math.ceil((rect.x+rect.width-1)/zonew)*zonew
-    local y2=math.ceil((rect.y+rect.height-1)/zoneh)*zoneh
-    -- pq(x1,y1,x2,y2)
-    rect = Rectangle(x1,y1,x2-x1,y2-y1)
-    -- pq(rect)
-  elseif gridtype=="slices" then
-    for i,slice in ipairs(sprite.slices) do
-      if rect:contains(slice.bounds) then
-        add(zones,{
-          name=slice.name,
-          bounds=slice.bounds,
-        })
-      end
-    end
-    return zones
-  end
-
-  for y=rect.y,rect.y+rect.height-1,zoneh do
-    for x=rect.x,rect.x+rect.width-1,zonew do
-      local name="aseprite"..getId()
-      local bounds=Rectangle{x=x,y=y,width=zonew,height=zoneh}
-      add(zones,{
-        name=name,
-        bounds=bounds,
-      })
-    end
-  end
-  return zones
-end
-
 -- https://github.com/dacap/export-aseprite-file/blob/master/export.lua#L125-L132
 local function get_tileset_for_layer(layer)
   for i,tileset in ipairs(layer.sprite.tilesets) do
-    if layer.tileset == tileset then
+    if layer.tileset==tileset then
       return tileset
     end
   end
@@ -223,7 +164,7 @@ end
 -- there's gotta be a better way to convert a tilemap cel into pixels, right?
 -- returns: an Image
 local function renderTilemapCel(cel)
-  local tilemap=cel.image
+  local tilemap = cel.image
   assert(tilemap.colorMode==ColorMode.TILEMAP)
   assert(cel.sprite.tilesets)
   assert(#cel.sprite.tilesets>0,"error: no tilesets found on a tilemap layer")
@@ -237,26 +178,88 @@ local function renderTilemapCel(cel)
   assert(tileset)
   local size = tileset.grid.tileSize
   local img = Image(tilemap.width*size.width,tilemap.height*size.height)
-  for ty=0,tilemap.height-1 do
-    for tx=0,tilemap.width-1 do
-      local fakepixel=tilemap:getPixel(tx,ty)
-      local tileix=app.pixelColor.tileI(fakepixel)
-      local tileimg=tileset:getTile(tileix)
-      img:drawImage(tileimg,tx*size.width,ty*size.height)
-    end
+  for it in tilemap:pixels() do
+    local tileix = app.pixelColor.tileI(it())
+    local tileimg = tileset:getTile(tileix)
+    img:drawImage(tileimg,it.x*size.width,it.y*size.height)
   end
   return img
 end
 
+--[[
+# script
+]]
+
+-- returns a list of "zones" that fit into the current selection of sprite
+--   a "zone" is a {name = <string>, bounds = <Rectangle>} object
+-- sel may be non-rectangular (e.g. multiple rectangles)
+--   but support isn't great (e.g. the grid anchor will not reset between
+--   multiple selections)
+local function gatherZones(sprite,gridtype)
+  local zones = {}
+
+  local sel = sprite.selection
+  -- rect: the subrectangle to export tiles from
+  local rect = sel.isEmpty and sprite.bounds or sel.bounds
+  local zonew
+  local zoneh
+  if gridtype=="5x5" then
+    zonew = 5
+    zoneh = 5
+    -- if your selection isn't exact multiples of 5, extra tiles will be created
+    -- on the edge. these tiles will be a full 5x5
+  elseif gridtype=="aseprite grid" then
+    zonew = sprite.gridBounds.width
+    zoneh = sprite.gridBounds.height
+    -- selection correction
+    -- pq(rect)
+    local x1 = math.floor(rect.x/zonew)*zonew
+    local y1 = math.floor(rect.y/zoneh)*zoneh
+    local x2 = math.ceil((rect.x+rect.width-1)/zonew)*zonew
+    local y2 = math.ceil((rect.y+rect.height-1)/zoneh)*zoneh
+    -- pq(x1,y1,x2,y2)
+    rect = Rectangle(x1,y1,x2-x1,y2-y1)
+    -- pq(rect)
+  elseif gridtype=="slices" then
+    for i,slice in ipairs(sprite.slices) do
+      if rect:contains(slice.bounds) then
+        add(zones,{
+          name = slice.name,
+          bounds = slice.bounds,
+        })
+      end
+    end
+    return zones
+  end
+
+  for y = rect.y,rect.y+rect.height-1,zoneh do
+    for x = rect.x,rect.x+rect.width-1,zonew do
+      local name = "aseprite"..getId()
+      add(zones,{
+        name = name,
+        bounds = Rectangle{
+          x = x,
+          y = y,
+          width = zonew,
+          height = zoneh,
+        },
+      })
+    end
+  end
+  return zones
+end
+
 -- create an image that we can extract colors from
+-- note: the image will have colorMode RGB, even if
+-- app.activeSprite is TILEMAP or INDEXED
 local function prepareImage(sprite,layeronly)
-  local img=Image(sprite.width,sprite.height) --iirc we want this instead of sprite.spec to drop ColorMode info and become RGB
+  local img = Image(sprite.width,sprite.height) --iirc we want this instead of sprite.spec to drop ColorMode info and become RGB
   if layeronly then
     if not app.activeCel then
       app.alert("error: current layer is empty")
       return
     end
-    if app.activeCel.image.colorMode == ColorMode.TILEMAP then
+    if app.activeCel.image.colorMode==ColorMode.TILEMAP then
       img:drawImage(renderTilemapCel(app.activeCel))
     else
       img:drawImage(app.activeCel.image, app.activeCel.position)
@@ -278,7 +281,7 @@ end
 --   0110.
 --   110..
 local function exportZones(img,zones)
-  local result={}
+  local result = {}
   for i,zone in ipairs(zones) do
     local str = exportZone(img,zone)
     if str then
@@ -309,16 +312,16 @@ end
 
 local dlg = Dialog("PuzzleScript Export")
 dlg:combobox{
-  id="gridtype",
-  label="grid type",
-  option=defaultGridType(),
-  options={ "5x5","aseprite grid","slices" },
+  id = "gridtype",
+  label = "grid type",
+  option = defaultGridType(),
+  options = { "5x5","aseprite grid","slices" },
 }
 dlg:check{
-  id="layeronly",
-  label="active layer only",
+  id = "layeronly",
+  label = "active layer only",
 }
-dlg:button{text="Export", onclick=function()
+dlg:button{text = "Export", onclick = function()
   local gridtype,layeronly = dlg.data.gridtype,dlg.data.layeronly
 
   if not app.activeSprite then return app.alert("error: no sprite found") end
@@ -330,18 +333,18 @@ dlg:button{text="Export", onclick=function()
   local label = string.format("output (%d)",#tiles)
   local text = table.concat(tiles,"\n")
   dlg:modify{
-    id="output",
-    label=label,
-    text=text,
-    focus=true,
-    visible=true,
+    id = "output",
+    label = label,
+    text = text,
+    focus = true,
+    visible = true,
   }
 end}
 dlg:entry{
-  id="output",
-  label="output",
-  text="",
-  focus=false,
-  visible=false,
+  id = "output",
+  label = "output",
+  text = "",
+  focus = false,
+  visible = false,
 }
-dlg:show{wait=false}
+dlg:show{wait = false}
